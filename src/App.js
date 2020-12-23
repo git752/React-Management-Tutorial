9c80124be8d597ffbd6f718ba5befded90cf93e2
akdlzmfh55 <akdlzmfh55@outlook.kr>
2020-12-23 13:06:30 +0900

    Create React Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Customer from './components/Customer'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const customers = [
+{
+  'id' : 1,
+  'image' : 'https://upload.wikimedia.org/wikipedia/commons/f/f9/Google_Lens_-_new_logo.png',
+  'name' : '홍길동',
+  'birthday' : '919119',
+  'gender' : '남자',
+  'job' : '대학생'
+},
+{
+  'id' : 2,
+  'image' : './3535r13.jpg',
+  'name' : '훙길둥',
+  'birthday' : '313141',
+  'gender' : '남자',
+  'job' : '대학생'
 }
+]
+
+
+class App extends Component{
+    render(){
+      return(
+        <div>
+          {
+            customers.map(c => {
+              return(
+              <Customer
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />);
+            })
+          }
+        </div>
+      );
+    }
+}
+
+
 
 export default App;
